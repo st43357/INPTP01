aee96c2d80206c6a2d7d75c3e9c653b710d3c4c4
D V <st43357@student.upce.cz>
2017-10-10 18:13:48 +0200

    First commit

--- INPTP_AppForFixing/Entity/Boss.cs
+++ INPTP_AppForFixing/Entity/Boss.cs
@@ -18,42 +18,79 @@ namespace INPTP_AppForFixing
             this.department = department;
         }
 
+
+        /// <summary>
+        /// Method which set salary bonus.
+        /// </summary>
+        /// <param name="salaryBonus">Value of salary bonus.</param>
         public void SetSalaryBonus(double salaryBonus)
         {
             perEmplSalaryBonus = salaryBonus;
         }
 
+
+        /// <summary>
+        /// Method on insert employee.
+        /// </summary>
+        /// <param name="empl">Employee which is add.</param>
         public void InsertEmpl(Employee empl)
         {
             employees.Add(empl);
         }
 
+        /// <summary>
+        /// Method on remove employee.
+        /// </summary>
+        /// <param name="empl">Employee which is remove.</param>
         public void PurgeEmpl(Employee empl)
         {
             employees.Remove(empl);
         }
 
+
+        /// <summary>
+        /// Method
+        /// </summary>
+        /// <param name="empl"></param>
+        /// <returns>Return true if employee is find. Else return false.  </returns>
         public bool HasEmployee(Employee empl)
         {
             return employees.Contains(empl);
         }
 
+
+        /// <summary>
+        /// Method which return set of employees.
+        /// </summary>
+        /// <returns>Return al employees.</returns>
         public ISet<Employee> GetEmployees()
         {
             return new HashSet<Employee>(employees);
         }
 
+
+        /// <summary>
+        /// Method which return count of employees.
+        /// </summary>
+        /// <returns>Return count of employees..</returns>
         public int EmployeeCount
         {
             get { return employees.Count; }
         }
 
-        // calculate base year salary + subemployee bonus
+        /// <summary>
+        /// Method which calculate base year salary and subemployee bonus.
+        /// </summary>
+        /// <returns>Return calculated value.</returns>
         public override double CalcYearlySalaryCZK()
         {
             return (base.CalcYearlySalaryCZK() + (EmployeeCount*perEmplSalaryBonus*12));
         }
 
+        /// <summary>
+        /// Method calculate yearly income of all employees.
+        /// </summary>
+        /// <returns>Return calculated value.</returns>
         public override double CalcYearlyIncome()
         {
             return base.CalcYearlyIncome() + (EmployeeCount*perEmplSalaryBonus * (1-TaxRate));
